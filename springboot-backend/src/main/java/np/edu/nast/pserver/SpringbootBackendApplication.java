f486b9fbdc1d41520ef9f0b6650c21aeb9a871e3
Ram Nath <yogiram727@gmail.com>
2026-01-29 07:39:42 +0545

    change in railway.toml and applicpro

--- springboot-backend/src/main/java/np/edu/nast/pserver/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/np/edu/nast/pserver/SpringbootBackendApplication.java
@@ -2,26 +2,33 @@ package np.edu.nast.pserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBackendApplication.class, args);
-	}
-	
-	@Configuration
-	@EnableWebMvc
-	public class WebConfig implements WebMvcConfigurer {
-
-	    @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**");
-	    }
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns(
+                            "https://*.netlify.app",
+                            "http://localhost:4200",
+                            "http://localhost:8080"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
 }
